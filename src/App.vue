0426b9d553f5e0d9cd7fbf0bdd60b0b3a68712ee
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-05-17 14:57:17 +0200

    mentions légales

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <header>
-    
-   <h1>Gououte</h1>
-  
-  </header>
+  <div class="h-screen w-screen overflow-x-hidden bg-gradient-to-b from-baby-blue to-blue-electric">
+    <header>
+      <h1 class="p-24 text-center font-quicksand text-2xl uppercase tracking-widest text-white">Mentions légales</h1>
+    </header>
+    <div class="rounded-3xl bg-white font-raleway text-dark-black">
+      <p class="mr-7 p-6 font-medium uppercase">Informations :</p>
+      <p class="ml-7 mr-7 mb-8">
+        Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux
+        utilisateurs du site Askme l'identité des différents intervenants dans le cadre de sa création et de son suivi.
+      </p>
 
- 
+      <p class="ml-7 mr-7 mb-8">
+        Askme est accessible à l'URL www.askme.com (le "Site"), est édité par : Askme, société au capital de 1000000 euros, immatriculée au
+        registre du commerce et des sociétés de PARIS sous le numéro 00000000, dont le siège social est situé 4 Place Tharradin, 25200
+        Montbéliard, représentée par Mélina BOUCHELLEGHEM, dûment habilitée.
+      </p>
 
-
+      <p class="ml-7 mr-7 mb-8">
+        Le numéro individuel de TVA de l'éditeur est : 00000000. Le Site est hébergé par la société Infomaniak, située Rue Eugène-Marziano
+        25, 1227 Genève, Suisse, (contact téléphonique ou email : (+41) 22 820 35 44). Les directeurs de la publication du Site sont Mélina
+        Bouchelleghem, David Sarkissian et Baptiste Rognon
+      </p>
+      <p class="ml-6 mr-6 pb-11">
+        Par téléphone : +00000000 <br />Par email : melina.bhlm@gmail.com<br />
+        Par courrier : 4 Place Tharradin, 25200 Montbéliard. <br /><br />Par téléphone : +00000000<br />
+        Par email :david.sarkissian@hotmail.com <br />Par courrier : 4 Place Tharradin, 25200 Montbéliard.<br /><br />
+        Par téléphone : +00000000 <br />Par email : baptiste.rognon@gmail.com <br />Par courrier : 4 Place Tharradin, 25200 Montbéliard.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  components: { },
+  components: {},
 };
 </script>
--- src/views/MentionsView.vue
+++ src/views/MentionsView.vue
@@ -1,4 +1,3 @@
 <template>
-    <h1>Mentions Légales</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt eveniet quia eligendi autem, dolores blanditiis temporibus explicabo error. Aspernatur tenetur fugit, quas reiciendis vero nulla dolorum alias illum quo ullam.s</p>
+  <h1>Test</h1>
 </template>
