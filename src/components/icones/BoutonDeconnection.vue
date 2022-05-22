4bbed7fd23330f2c09457e52fe68b20684ee17aa
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2022-05-22 16:01:44 +0200

    Page profil (bouton déconnection à revoir)

---
+++ src/components/icones/BoutonDeconnection.vue
@@ -0,0 +1,9 @@
+<template>
+    
+<svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M31 31L42 20L31 9" stroke="white" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M14 42H5.33333C4.18406 42 3.08186 41.52 2.2692 40.6657C1.45655 39.8114 1 38.6527 1 37.4444V5.55556C1 4.34735 1.45655 3.18862 2.2692 2.33429C3.08186 1.47996 4.18406 1 5.33333 1H14" stroke="white" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M41 20H14" stroke="white" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+</template>>
--- src/views/ProfilView.vue
+++ src/views/ProfilView.vue
@@ -1,3 +1,83 @@
 <template>
-  <h2>Test</h2>
+  <div class="h-screen w-screen overflow-x-hidden bg-gradient-to-b from-baby-blue to-blue-electric">
+    
+      <div class= "text-center">
+        <h2 class=" mt-9 inline-block border-b-2 font-quicksand text-xl font-extralight text-white uppercase px-4">Profil</h2>
+      </div>
+    <div class="mx-6 my-6  flex justify-center">
+      <IconeProfile class="w-32"></IconeProfile>
+    </div>
+
+    <div class="text-center">
+      
+
+      <div class="mx-8 rounded-3xl bg-white md:grid md:grid-cols-2 ">
+        <div class="flex items-center justify-evenly border-b-2 md:border-r-2">
+            <IconeProfile class="opacity-0 absolute 375p:opacity-100 375p:relative"></IconeProfile>
+                <div class="flex flex-col items-start mx-2">
+                        <p class="font-quicksand">Martin</p>
+                        <p class="text-left text-xs 425p:text-base text-gris_surligner font-raleway ">Lorem ipsum dolor sit amet.</p>
+                        <p class="text-left text-xs text-gris_surligner font-raleway ">Envoyé  auj à 13:57</p>
+                </div>
+                    <RouterLink to="/tchat"><FlecheDroit class=" w-8"></FlecheDroit></RouterLink> 
+        </div>
+          
+        <div class="flex items-center justify-evenly border-b-2">
+            <IconeProfile class="opacity-0 absolute 375p:opacity-100 375p:relative"></IconeProfile>
+                <div class="flex flex-col items-start mx-2">
+                        <p class="font-quicksand">Léa</p>
+                        <p class="text-left text-xs 425p:text-base text-gris_surligner font-raleway ">Lorem ipsum dolor sit amet.</p>
+                        <p class="text-left text-xs text-gris_surligner font-raleway ">Envoyé à l'instant</p>
+                </div>
+                    <RouterLink to="/tchat"><FlecheDroit class=" w-8"></FlecheDroit></RouterLink> 
+        </div>
+
+        <div class="flex items-center justify-evenly md:border-r-2 ">
+            <IconeProfile class="opacity-0 absolute 375p:opacity-100 375p:relative"></IconeProfile>
+                <div class="flex flex-col items-start mx-2">
+                        <p class="font-quicksand">Anonyme</p>
+                        <p class="text-left text-xs 425p:text-base text-gris_surligner font-raleway ">Lorem ipsum dolor sit amet.</p>
+                        <p class="text-left text-xs text-gris_surligner font-raleway ">Envoyé hier</p>
+                </div>
+                    <RouterLink to="/tchat"><FlecheDroit class=" w-8"></FlecheDroit></RouterLink> 
+        </div>
+
+        
+      </div>
+      
+  <RouterLink to="/" class="flex justify-center mt-5"><BoutonDeconnection class="w-7"></BoutonDeconnection></RouterLink>
+    <div>
+      <menuu class="mt-8"></menuu>
+    </div>     
+    </div>
+  </div>
 </template>
+
+<script>
+import IconeProfile from '../components/icones/IconeProfile.vue';
+import BoutonFleche from '../components/icones/BoutonFleche.vue';
+import Menuu from '../components/Menu.vue'
+import FlecheDroit from '../components/icones/FlecheDroit.vue';
+import BoutonDeconnection from '../components/icones/BoutonDeconnection.vue';
+
+
+export default {
+  name: "App",
+  components: {
+    IconeProfile,
+    BoutonFleche,
+    Menuu,
+    FlecheDroit,
+    BoutonDeconnection
+    },
+};
+</script>
+
+
+
+
+
+
+
+
+
