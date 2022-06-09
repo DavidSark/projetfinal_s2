3df08409d9ec2cda42b80631feceba8b288e1e8c
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-06-09 11:38:16 +0200

    boutton rechercher

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -2,8 +2,6 @@
   <div class="h-screen w-screen overflow-x-hidden bg-gradient-to-b from-baby-blue to-blue-electric">
     <div class="mx-6 my-6 flex justify-center">
       <IconeProfile class="w-14"></IconeProfile>
-
-     
     </div>
     <div class="flex flex-col">
       <RouterLink to="/connexion">Connexion</RouterLink>
@@ -12,7 +10,6 @@
 
     <div class="mb-8 text-center font-quicksand text-white">
       <p class="text-xl">Bienvenue sur Askme !</p>
-      <p class="text-sm text-gris">Tagline?</p>
     </div>
 
     <div class="text-center">
@@ -22,13 +19,32 @@
 
       <div class="mx-8 rounded-3xl bg-gris pb-6">
         <div>
-          <input class="mt-4" v-model="filter"/>
-         <tbody>
-                    <tr v-for="part in filterByNom" :key="part.id">
-                      <td><RouterLink :to="{ name:'Apercu', params: { id: part.id }}">{{part.nom}}</RouterLink></td>
-                      <p>{{part.desc}}</p>                 
-                    </tr>
-                </tbody>
+          <input
+            class="
+              mt-5
+              border-b-2
+              border-gris_surligner
+              font-raleway
+              text-gris_surligner
+              placeholder-gris_surligner
+              outline-none
+              focus:border-indigo-500
+            "
+            placeholder="Rechercher..."
+            v-model="filter"
+          />
+          <tbody>
+            <div class="bg-white">
+              <tr v-for="part in filterByNom" :key="part.id">
+                <td>
+                  <div class="mb-5">
+                    <RouterLink :to="{ name: 'Apercu', params: { id: part.id } }">{{ part.nom }}</RouterLink>
+                  </div>
+                </td>
+                <p>{{ part.desc }}</p>
+              </tr>
+            </div>
+          </tbody>
         </div>
       </div>
 
@@ -49,26 +65,22 @@ import CardDemandeDroit from "../components/CardDemandeDroit.vue";
 
 import Menuu from "../components/Menu.vue";
 
-import { 
-    getFirestore, 
-    collection, 
-    onSnapshot, 
-    query,
-    where,
-    orderBy
-} from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js'
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  query,
+  where,
+  orderBy,
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
 
 // Fonctions Storage
-import { 
-    getStorage, 
-    ref, 
-    getDownloadURL, 
-} from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js'
+import { getStorage, ref, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js";
 
 // Fonction authentification
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js'
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js";
 
-import { emitter } from '../main.js';
+import { emitter } from "../main.js";
 
 export default {
   name: "App",
@@ -79,108 +91,104 @@ export default {
     Menuu,
   },
 
-
-   data(){
-    return{
+  data() {
+    return {
       user: {
         email: null,
         password: null,
       },
-      userInfo:null,      // Informations complémentaires user connecté
-      name:"Vidéo",       // Titre de l'application ou nom du user
-      avatar:null,        // Avatar / image du user connecté
-      isAdmin:false,       // Si l'utilisateur est ou non administrateur
-      listeParticipant:[],
-      nom:null, 
-      listeArtisteSynchro:[],
-      filter:''
-
-    }
+      userInfo: null, // Informations complémentaires user connecté
+      name: "Vidéo", // Titre de l'application ou nom du user
+      avatar: null, // Avatar / image du user connecté
+      isAdmin: false, // Si l'utilisateur est ou non administrateur
+      listeParticipant: [],
+      nom: null,
+      listeArtisteSynchro: [],
+      filter: "",
+    };
   },
 
-   computed:{
-      //Tri des catégories par ordre alpha 
-      orderByNom:function(){
-        return this.listeArtisteSynchro.sort(function(a,b){
-          if (a.nom < b.nom) return -1; 
-          if (a.nom > b.nom) return 1;
-          return 0;
-        })
-      },
+  computed: {
+    //Tri des catégories par ordre alpha
+    orderByNom: function () {
+      return this.listeArtisteSynchro.sort(function (a, b) {
+        if (a.nom < b.nom) return -1;
+        if (a.nom > b.nom) return 1;
+        return 0;
+      });
+    },
 
-      filterByNom:function(){
-        if(this.filter.length > 0){
-          let filter = this.filter.toLowerCase();
-          return this.orderByNom.filter(function(groupe){
-            return groupe.nom.toLowerCase().includes(filter);
-          })
-        }
-        else{
-          return this.orderByNom;
-        }
+    filterByNom: function () {
+      if (this.filter.length > 0) {
+        let filter = this.filter.toLowerCase();
+        return this.orderByNom.filter(function (groupe) {
+          return groupe.nom.toLowerCase().includes(filter);
+        });
+      } else {
+        return this.orderByNom;
       }
+    },
   },
 
-  mounted(){
+  mounted() {
     this.getUserConnect();
-    emitter.on('connectUser', e => {
+    emitter.on("connectUser", (e) => {
       this.user = e.user;
-      console.log ('App => Reception user connecté', this.user);
+      console.log("App => Reception user connecté", this.user);
       this.getUserInfo(this.user);
-    })
-    emitter.on('deConnectUser', e => {
+    });
+    emitter.on("deConnectUser", (e) => {
       this.user = e.user;
-      console.log('App => Reception user déconnecté', this.user);
+      console.log("App => Reception user déconnecté", this.user);
       this.userInfo = null;
-      this.name = 'Vidéo';
+      this.name = "Vidéo";
       this.avatar = null;
       this.isAdmin = false;
-    })
+    });
 
-     this.getParticipants();
-     this.getArtisteSynchro();
-
+    this.getParticipants();
+    this.getArtisteSynchro();
   },
-  methods:{
-     async getArtisteSynchro(){
-                // Obtenir Firestore
-                const firestore = getFirestore();
-                // Base de données (collection)  document pays
-                const dbArtiste= collection(firestore, "demandes");
-                // Liste des pays synchronisée
-                const query = await onSnapshot(dbArtiste, (snapshot) =>{
-                    //  Récupération des résultats dans listePaysSynchro
-                    // On utilse map pour récupérer l'intégralité des données renvoyées
-                    // on identifie clairement le id du document
-                    // les rest parameters permet de préciser la récupération de toute la partie data
-                    this.listeArtisteSynchro = snapshot.docs.map(doc => ({id:doc.id, ...doc.data()}));
-                })
-            },
-            
-     async getParticipants(){
-            // Obtenir Firestore
-            const firestore = getFirestore();
-            // Base de données (collection)  document participant
-            const dbPart = collection(firestore, "demandes");
-            // Liste des participants triés sur leur nom
-            const q = query(dbPart, orderBy('nom', 'asc'));
-            await onSnapshot(q, (snapshot) => {
-                this.listeParticipant = snapshot.docs.map(doc => (
-                    {id:doc.id, ...doc.data()}
-                ))
-                // Récupération des images de chaque participant
-                // parcours de la liste
-               })      
-        },
-     async getUserConnect(){
-      await getAuth().onAuthStateChanged(function(user){
-        if(user){
-          this.user = user;
-          this.getUserInfo(this.user);
-        }
-      }.bind(this));
+  methods: {
+    async getArtisteSynchro() {
+      // Obtenir Firestore
+      const firestore = getFirestore();
+      // Base de données (collection)  document pays
+      const dbArtiste = collection(firestore, "demandes");
+      // Liste des pays synchronisée
+      const query = await onSnapshot(dbArtiste, (snapshot) => {
+        //  Récupération des résultats dans listePaysSynchro
+        // On utilse map pour récupérer l'intégralité des données renvoyées
+        // on identifie clairement le id du document
+        // les rest parameters permet de préciser la récupération de toute la partie data
+        this.listeArtisteSynchro = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      });
     },
-    async getUserInfo(user){
+
+    async getParticipants() {
+      // Obtenir Firestore
+      const firestore = getFirestore();
+      // Base de données (collection)  document participant
+      const dbPart = collection(firestore, "demandes");
+      // Liste des participants triés sur leur nom
+      const q = query(dbPart, orderBy("nom", "asc"));
+      await onSnapshot(q, (snapshot) => {
+        this.listeParticipant = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        // Récupération des images de chaque participant
+        // parcours de la liste
+      });
+    },
+    async getUserConnect() {
+      await getAuth().onAuthStateChanged(
+        function (user) {
+          if (user) {
+            this.user = user;
+            this.getUserInfo(this.user);
+          }
+        }.bind(this)
+      );
+    },
+    async getUserInfo(user) {
       // Rechercher les informations complémentaires de l'utilisateur
       // Obtenir Firestore
       const firestore = getFirestore();
@@ -189,27 +197,25 @@ export default {
       // Recherche du user par son uid
       const q = query(dbUsers, where("uid", "==", user.uid));
       await onSnapshot(q, (snapshot) => {
-          this.userInfo = snapshot.docs.map(doc => (
-            {id:doc.id, ...doc.data()}
-          ));
-          console.log("userInfo", this.userInfo);                
-          // userInfo étant un tableau, onn récupère
-          // ses informations dans la 1° cellule du tableau : 0
-          this.name = this.userInfo[0].login;
-          this.isAdmin=this.userInfo[0].admin;
-          // Recherche de l'image du user sur le Storage
-          const storage = getStorage();
-          // Référence du fichier avec son nom
-          const spaceRef = ref(storage, 'users/'+this.userInfo[0].avatar);
-          getDownloadURL(spaceRef)
-            .then((url) => {
-              this.avatar = url;
-            })
-            .catch((error) =>{
-              console.log('erreur downloadUrl', error);
-            })
-        });
-      }
-  }
+        this.userInfo = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        console.log("userInfo", this.userInfo);
+        // userInfo étant un tableau, onn récupère
+        // ses informations dans la 1° cellule du tableau : 0
+        this.name = this.userInfo[0].login;
+        this.isAdmin = this.userInfo[0].admin;
+        // Recherche de l'image du user sur le Storage
+        const storage = getStorage();
+        // Référence du fichier avec son nom
+        const spaceRef = ref(storage, "users/" + this.userInfo[0].avatar);
+        getDownloadURL(spaceRef)
+          .then((url) => {
+            this.avatar = url;
+          })
+          .catch((error) => {
+            console.log("erreur downloadUrl", error);
+          });
+      });
+    },
+  },
 };
 </script>
