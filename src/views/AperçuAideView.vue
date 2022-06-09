6afb70d5769c21a4cec1c4b1e23a4bd9d1994cd7
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-06-09 13:13:30 +0200

    front end page apercu

--- src/views/AperçuAideView.vue
+++ src/views/AperçuAideView.vue
@@ -1,70 +1,58 @@
 <template>
   <div class="h-screen w-screen overflow-x-hidden bg-gradient-to-b from-baby-blue to-blue-electric">
-    <div class="mx-6 my-6  flex justify-center">
+    <div class="mx-6 my-6 flex justify-center">
       <IconeProfile class="w-14"></IconeProfile>
     </div>
 
     <div class="text-center">
       <div class="">
-        <h2 class="inline-block border-b-2 font-quicksand text-xl font-extralight text-white uppercase px-4">Aperçu</h2>
+        <h2 class="inline-block border-b-2 px-4 font-quicksand text-xl font-extralight uppercase text-white">Aperçu</h2>
       </div>
 
-      <div class="mx-8 rounded-3xl bg-white pb-6 ">
-        
+      <div class="mx-8 rounded-3xl bg-white pb-6">
+        <div class="mt flex flex-col items-center justify-center">
+          <div class="mx-5 mt-5 text-left font-raleway text-gris_surligner 375p:text-sm 425p:text-base">
+            <td class="text-center"></td>
 
-        <div class="flex justify-center flex-col items-center mt">
-          
-         
-          <div class="text-left mx-5 mt-5 text-gris_surligner font-raleway">
-                        <td class="text-center">                                        
-
-                        </td>
-                        <p>{{participant.nom}}</p>
-                         <p>{{participant.desc}}</p>
-                          <p>{{participant.text}}</p>
-       
+            <p class="text-center font-quicksand font-semibold uppercase text-dark-black">{{ participant.nom }}</p>
+            <p class="text-center italic">{{ participant.desc }}</p>
+            <p class="p-5">{{ participant.text }}</p>
           </div>
-         <p class="font-raleway text-gris_surligner mt-4">Aider !</p>
+
           <RouterLink to="/tchat"><BoutonFleche class="w-16"></BoutonFleche></RouterLink>
-          
-          </div>
-          
+        </div>
       </div>
 
-     <div class="w-screen bg-soft-beige h-32 my-9">
-       
-     </div>
+      <div class="my-9 h-32 w-screen bg-soft-beige"></div>
 
-    <div>
-      <menuu></menuu>
-    </div>     
+      <div>
+        <menuu></menuu>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import IconeProfile from '../components/icones/IconeProfile.vue';
-import BoutonFleche from '../components/icones/BoutonFleche.vue';
-import Menuu from '../components/Menu.vue'
+import IconeProfile from "../components/icones/IconeProfile.vue";
+import BoutonFleche from "../components/icones/BoutonFleche.vue";
+import Menuu from "../components/Menu.vue";
 
-import { 
-    getFirestore,   // Obtenir le Firestore
-    collection,     // Utiliser une collection de documents
-    onSnapshot,     // Demander une liste de documents d'une collection, en les synchronisant
-    query,          // Permet d'effectuer des requêtes sur Firestore
-    orderBy,
-    doc,
-    getDoc,        // Permet de demander le tri d'une requête query
-} from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js'
-
+import {
+  getFirestore, // Obtenir le Firestore
+  collection, // Utiliser une collection de documents
+  onSnapshot, // Demander une liste de documents d'une collection, en les synchronisant
+  query, // Permet d'effectuer des requêtes sur Firestore
+  orderBy,
+  doc,
+  getDoc, // Permet de demander le tri d'une requête query
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
 
 // Cloud Storage : import des fonctions
-import { 
-    getStorage,             // Obtenir le Cloud Storage
-    ref,                    // Pour créer une référence à un fichier à uploader
-    getDownloadURL,         // Permet de récupérer l'adress complète d'un fichier du Storage
-} from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js'
-
+import {
+  getStorage, // Obtenir le Cloud Storage
+  ref, // Pour créer une référence à un fichier à uploader
+  getDownloadURL, // Permet de récupérer l'adress complète d'un fichier du Storage
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js";
 
 export default {
   name: "App",
@@ -72,64 +60,55 @@ export default {
     IconeProfile,
     BoutonFleche,
     Menuu,
-    },
-    name:'ListeView',
+  },
+  name: "ListeView",
   data() {
     return {
-            listeParticipant:[],
-              participant:{           
-                nom:null,
-                desc:null,
-                text:null,  },
-            refParticipant:null,     
-            photoActuelle:null 
+      listeParticipant: [],
+      participant: {
+        nom: null,
+        desc: null,
+        text: null,
+      },
+      refParticipant: null,
+      photoActuelle: null,
+    };
+  },
+  mounted() {
+    // Montage de la vue
+    // Appel de la liste des participants
+    this.getParticipants();
+    console.log("id participant", this.$route.params.id);
+    this.getParticipant(this.$route.params.id);
+  },
+  methods: {
+    async getParticipant(id) {
+      const firestore = getFirestore();
+      const docRef = doc(firestore, "demandes", id);
+      this.refParticipant = await getDoc(docRef);
+      if (this.refParticipant.exists()) {
+        this.participant = this.refParticipant.data();
+        this.photoActuelle = this.participant.text;
+      } else {
+        this.console.log("Participant inexistant");
+      }
+    },
 
-        }
+    async getParticipants() {
+      // Obtenir Firestore
+      const firestore = getFirestore();
+      // Base de données (collection)  document participant
+      const dbPart = collection(firestore, "demandes");
+      // Liste des participants triés sur leur nom
+      const q = query(dbPart, orderBy("nom", "asc"));
+      await onSnapshot(q, (snapshot) => {
+        this.listeParticipant = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        // Récupération des images de chaque participant
+        // parcours de la liste
+      });
     },
-    mounted(){ // Montage de la vue
-        // Appel de la liste des participants
-        this.getParticipants();
-        console.log("id participant", this.$route.params.id);     
-      this.getParticipant(this.$route.params.id);
-    },
-    methods: {
-      async getParticipant(id){
-           
-            const firestore = getFirestore();
-            const docRef = doc(firestore, "demandes", id);
-            this.refParticipant = await getDoc(docRef);
-            if(this.refParticipant.exists()){
-                this.participant = this.refParticipant.data();
-                this.photoActuelle = this.participant.text;
-            }else{
-                this.console.log("Participant inexistant");
-            }
-        },
-        
-        async getParticipants(){
-            // Obtenir Firestore
-            const firestore = getFirestore();
-            // Base de données (collection)  document participant
-            const dbPart = collection(firestore, "demandes");
-            // Liste des participants triés sur leur nom
-            const q = query(dbPart, orderBy('nom', 'asc'));
-            await onSnapshot(q, (snapshot) => {
-                this.listeParticipant = snapshot.docs.map(doc => (
-                    {id:doc.id, ...doc.data()}
-                ))
-                // Récupération des images de chaque participant
-                // parcours de la liste
-               })      
-        },
-    }
+  },
 };
 </script>
-
-
-
-
-
-
-
 
 
